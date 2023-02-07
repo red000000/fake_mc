69e1c791fbc33f1555d9a20057f9fb2f8f547c2e
qwq <Qq3208216735@outlook.com>
2023-02-07 18:21:42 +0800

    地图进化版1.0

--- mc-/mc-/box_class.cpp
+++ mc-/mc-/box_class.cpp
@@ -1,9 +1,10 @@
 #include"class.h"
+#include"map_create.h"
 box::box()//默认为10
 {
-	this->box_long = 10;
-	this->box_wide = 10;
-	this->box_high = 10;
+	this->box_long = box_size;
+	this->box_wide = box_size;
+	this->box_high = box_size;
 	this->id = 0;//简单初始化
 }
 void box::box_set(int box_long, int box_wide, int box_high)
@@ -13,15 +14,3 @@ void box::box_set(int box_long, int box_wide, int box_high)
 	this->box_high = box_high;
 }
 //分割线---------
-grass::grass()
-{
-	this->name = "草方块";
-	this->id = 1;
-	this->Excavate_Level_Need = 0;
-}
-stone::stone()
-{
-	this->name = "石头";
-	this->id = 2;
-	this->Excavate_Level_Need = 1;
-}
--- mc-/mc-/源.cpp
+++ mc-/mc-/源.cpp
@@ -1,21 +1,19 @@
-#include<iostream>
+ï»¿#include<iostream>
 #include<vector>
 #include"class.h"
 #include"map_create.h"
 int main()
 {
-	std::vector<std::vector<std::vector<int>>> map_id = map_id_create(map_long, map_wide, map_high);//´´½¨µØÍ¼
-	/*for (int j = 0; j < map_wide; ++j)
+	std::vector<std::vector<std::vector<int>>> map_id = map::map_id_create(map_long, map_wide, map_high);//åˆ›å»ºåœ°å›¾
+	std::vector<box> box_data = map::box_data();
+	/*
+	for (int j = 0; j < map_wide; ++j)
 	{
 		for (int i = 0; i < map_long; ++i)
 			std::cout << map_id[i][j][50] << " ";
 		std::cout << std::endl;
-	}
-	//´òÓ¡Êý×ÖµØÍ¼.jpg
-	*/
-	grass a;
-	std::cout << a.box_long << a.box_high << a.id << std::endl;
-	a.box_set(1, 2, 3);
-	std::cout << a.box_long << a.box_high << a.id << std::endl;
+	}*/
+
+	//æ‰“å°æ•°å­—åœ°å›¾.jpg
 	return 0;
 }
