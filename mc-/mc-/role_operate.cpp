63189abebcf053f95e5c4f3d805033288be89241
frorab <2816277979@qq.com>
2023-02-08 12:01:43 +0800

    222

--- mc-/mc-/role_operate.cpp
+++ mc-/mc-/role_operate.cpp
@@ -14,4 +14,22 @@ void steve::move()
 	int y = rand() % 1000 + 1;
 	int z = 500;
 	std::cout << x << "    " << y << "    " << z <<"          \r";
+	void steve::move() {
+		while (1) {
+			if (GetKeyState(87) < 0 || GetKeyState(119) < 0) {
+				x += 5;
+			}
+			if (GetKeyState(83) < 0 || GetKeyState(115)) {
+				x -= 5;
+			}
+			if (GetKeyState(65) < 0 || GetKeyState(97)) {
+				y += 5;
+			}
+			if (GetKeyState(68) < 0 || GetKeyState(100)) {
+				y -= 5;
+			}
+			sleep(100);
+			std::cout << x << "    " << y << "    " << z << "    " << "    /r";
+		}
+	}
 }
